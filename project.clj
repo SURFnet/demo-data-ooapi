d4fb68b126f4467c80c697e7a2a75e307d560025
Joost Diepenmaat <joost@zeekat.nl>
2021-12-10 09:02:25 +0100

    Remove log4j dependency because of security issues.
    Switch to logback. Log4J default configuration has a giant remote code
    exection vulnerability.
    
    See also https://www.cert.govt.nz/it-specialists/advisories/log4j-rce-0-day-actively-exploited/

--- project.clj
+++ project.clj
@@ -4,13 +4,12 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :url "https://github.com/SURFnet/demo-data-ooapi"
   :dependencies [[camel-snake-kebab "0.4.1"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [hiccup "1.0.5"]
-                 [log4j/log4j "1.2.17"]
                  [nl.surf/demo-data "1.0.0"]
                  [nl.zeekat/ring-openapi-validator "0.1.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.5.0"]
-                 [org.slf4j/slf4j-log4j12 "1.7.30"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [ring/ring-codec "1.1.2"]
                  [ring/ring-jetty-adapter "1.8.0"]
                  [ring/ring-json "0.5.0"]
